36287810212c5e286e090a38fd933f0108bc5876
jlsn5 <jlsn5@gcloud.ua.es>
2022-06-30 17:29:05 +0200

    empezando react

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App(){
+    return (<h1>Hola Mundo!</h1>);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
+
+
+
+
